9d5633dcfe04d827a919a6ba1cc493e569a08444
davidade300 <iaderinho@icloud.com>
2024-11-27 12:36:15 -0300

    .a

--- architecture_patterns_python/orm.py
+++ architecture_patterns_python/orm.py
@@ -1,6 +1,15 @@
-from sqlalchemy import Column, Integer, MetaData, String, Table
-from sqlalchemy.orm import registry
+from sqlalchemy import (
+    Column,
+    Date,
+    ForeignKey,
+    Integer,
+    MetaData,
+    String,
+    Table,
+)
+from sqlalchemy.orm import registry, relationship
 from architecture_patterns_python.model import (
+    Batch,
     OrderLine,
 )  # the ORM imports (depends/knows about) the domail model, not the other way around
 
@@ -17,9 +26,37 @@ order_lines = Table(  # We define our databases tables and columns by using SQLA
     Column("orderid", String(255)),
 )
 
+batches = Table(
+    "batches",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("reference", String(255)),
+    Column("sku", String(255)),
+    Column("_purchased_quantity", Integer, nullable=False),
+    Column("eta", Date, nullable=True),
+)
+
+allocations = Table(
+    "allocations",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("orderline_id", ForeignKey("order_lines.id")),
+    Column("batch_id", ForeignKey("batches.id")),
+)
+
 
 # When called, the mapper function does its magic to bind our domain model classes
 # to the various tables we defined
 def start_mappers():
-    # lines_mapper = mapper_registry.map_declaratively(model.OrderLine, order_lines)
-    mapper_registry.map_imperatively(OrderLine, order_lines)
+    lines_mapper = mapper_registry.map_imperatively(OrderLine, order_lines)
+    mapper_registry.map_imperatively(
+        Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                lines_mapper,
+                secondary=allocations,
+                collection_class=set,
+            )
+        },
+    )
--- architecture_patterns_python/repository.py
+++ architecture_patterns_python/repository.py
@@ -1,12 +1,26 @@
 import abc
-import architecture_patterns_python.model as model
+from architecture_patterns_python.model import Batch
 
 
 class AbstractRepository(abc.ABC):
     @abc.abstractmethod
-    def add(self, batch: model.Batch):
+    def add(self, batch: Batch):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get(self, reference) -> model.Batch:
+    def get(self, reference) -> Batch:
         raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session) -> None:
+        self.session = session
+
+    def add(self, batch: Batch):  # TODO: check need to remove the hint later
+        self.session.add(batch)
+
+    def get(self, reference) -> Batch:  # TODO check need to remove return type
+        return self.session.query(Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(Batch).all()
